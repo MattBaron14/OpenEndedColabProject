6aa554dd754b43302f91e63b0f01150cbc388c42
Nicholas Lembo <nick@t60.home>
2018-07-31 12:07:47 -0400

    made it black and white

--- copy.sh
+++ copy.sh
@@ -4,3 +4,5 @@ rsync index.php /var/www/html/index.php
 rsync wilderness.jpg /var/www/html/wilderness.jpg
 rsync script.js /var/www/html/script.js
 rsync server.php /var/www/html/server.php
+rsync raycaster.html /var/www/html/raycaster.html
+rsync style.css /var/www/html/style.css
